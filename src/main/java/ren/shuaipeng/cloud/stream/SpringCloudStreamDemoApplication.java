162f95f52b34b43b4099ecd37a324e33b225ab31
after-thirty <after_thirty@126.com>
2022-05-23 17:11:52 +0800

    initial commit

---
+++ src/main/java/ren/shuaipeng/cloud/stream/SpringCloudStreamDemoApplication.java
@@ -0,0 +1,13 @@
+package ren.shuaipeng.cloud.stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudStreamDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ren/shuaipeng/cloud/stream/SpringCloudStreamDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ren.shuaipeng.cloud.stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStreamDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
